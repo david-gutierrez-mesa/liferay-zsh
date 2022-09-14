fe1da9a3e46026364ea76468f03edf20b621034f
David Gutierrez <david.gutierrez@liferay.com>
2022-09-14 14:40:41 +0200

    ZSH-0069 Add script to create subtask in Jira

---
+++ python-scripts/createTestingSubtaskInLPS.py
@@ -0,0 +1,55 @@
+from jira import JIRA
+import manageCredentialsCrypto
+
+if __name__ == "__main__":
+    login = manageCredentialsCrypto.get_credentials()
+    jira = JIRA("https://issues.liferay.com", basic_auth=login)
+    stories_without_testing_subtask = jira.search_issues(
+        'project = LPS AND (component in (componentsLeadByUser(team-echo))) AND issuetype = Story AND '
+        'status in (\"In Development\", \"In QA\", \"In PM Review\", \"Ready for QA\", \"Ready for Product Validation\") AND '
+        '(labels not in (echo-board-out) OR labels is EMPTY) AND'
+        '(issueFunction not in parentsOf(\"summary ~ \\\"Test Scenarios Coverage | Backend\\\"\") or '
+        'issueFunction not in parentsOf(\"summary ~ \\\"Test Scenarios Coverage | Frontend\\\"\"))')
+    for story in stories_without_testing_subtask:
+        needs_backend = True
+        needs_frontend = True
+        for subtask in story.fields.subtasks:
+            summary = subtask.fields.summary
+            if summary == 'Test Scenarios Coverage | Backend':
+                needs_backend = False
+            elif summary == 'Test Scenarios Coverage | Frontend':
+                needs_frontend = False
+
+        components = []
+        for component in story.fields.components:
+            components.append({'name': component.name})
+
+        if needs_backend:
+            subtask_backend = {
+                'project': {'key': 'LPS'},
+                'summary': 'Test Scenarios Coverage | Backend',
+                'description': '* Fill the Backend coverage on the test scenarios table, created in the parent story.\n'
+                               '* Implement the Backend unit and/or integration tests that are missing, '
+                               'comparing with the test scenarios table, created in the parent story.',
+                'issuetype': {'name': 'Technical Testing'},
+                'components': components,
+                'parent': {'id': story.id},
+            }
+            child = jira.create_issue(fields=subtask_backend)
+            print("Created sub-task: " + child.key)
+
+        if needs_frontend:
+            subtask_frontend = {
+                'project': {'key': 'LPS'},
+                'summary': 'Test Scenarios Coverage | Frontend',
+                'description': '* Fill the Frontend coverage on the test scenarios table, '
+                               'created in the parent story.\n'
+                               '* Implement the Backend unit and/or integration tests that are missing, '
+                               'comparing with the test scenarios table, created in the parent story.',
+                'issuetype': {'name': 'Technical Testing'},
+                'components': components,
+                'parent': {'id': story.id},
+            }
+            child = jira.create_issue(fields=subtask_frontend)
+            print("Created sub-task: " + child.key)
+
